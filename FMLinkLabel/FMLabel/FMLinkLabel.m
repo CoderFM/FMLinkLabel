47a672873269e41ee8bdee42b9ae6d9ef7bec0be
CoderFM <zhoufaming251@163.com>
2016-09-29 19:16:34 +0800

    增加注释

--- FMLinkLabel/FMLabel/FMLinkLabel.m
+++ FMLinkLabel/FMLabel/FMLinkLabel.m
@@ -9,11 +9,17 @@
 #import "FMLinkLabel.h"
 
 @interface FMLinkLabel ()<UITextViewDelegate>
-
+/**
+ *  存储点击事件的数组
+ */
 @property(nonatomic, strong)NSMutableArray *clickItems;
-
+/**
+ *  存储点击图片的字典
+ */
 @property(nonatomic, strong)NSMutableDictionary *clickAttachmentItems;
-
+/**
+ *  属性textView
+ */
 @property(nonatomic, weak)UITextView *textView;
 
 @end
@@ -31,7 +37,9 @@
 - (void)awakeFromNib{
     [self setUp];
 }
-
+/**
+ *  初始化
+ */
 - (void)setUp{
     self.clickItems = [NSMutableArray array];
     self.clickAttachmentItems = [NSMutableDictionary dictionary];
@@ -40,27 +48,32 @@
     self.textView.font = self.font;
     self.textView.frame = CGRectMake(-5, -8, self.bounds.size.width + 10,  self.bounds.size.height + 18);
 }
-
+/**
+ *  添加一个点击事件
+ *
+ *  @param item 传入点击事件
+ */
 - (void)addClickItem:(FMLinkLabelClickItem *)item{
-    
+    // 循环遍历每一个字符的范围, 防止换行导致的部分不能响应
     for (int i = 0; i < item.range.length; i++) {
         
         NSRange range = NSMakeRange(item.range.location + i, 1);
-        
+        // 设置TextView的选中范围
         self.textView.selectedRange = range;
-        
+        // 获取选中范围在textView上的尺寸
         CGRect rect = [self.textView firstRectForRange:self.textView.selectedTextRange];
-        
+        // 将选中范围清空
         self.textView.selectedRange = NSMakeRange(0, 0);
-        
+        // 转换坐标系到本身上来
         CGRect textRect = [self.textView convertRect:rect toView:self];
         
+        // 有点不准确, textView设置内容的时候container有偏移量吧  具体不太清楚
         NSInteger remainder = (NSInteger)textRect.origin.y % (NSInteger)self.font.lineHeight;
         
         if (remainder > 0) {
             textRect.origin.y += (self.font.lineHeight - remainder);
         }
-        
+        // 加入这个尺寸到数组 方便判断
         [item.textRects addObject:[NSValue valueWithCGRect:textRect]];
         
     }
@@ -69,14 +82,14 @@
 }
 
 - (void)addClickText:(NSString *)text attributeds:(NSDictionary *)attributeds transmitBody:(id)transmitBody clickItemBlock:(FMLinkLabelClickItemBlock)clickBlock{
-    
+    // 根据现有的文本生成可变的富文本
     NSMutableAttributedString *attr = nil;
     if (self.attributedText) {
         attr = [self.attributedText mutableCopy];
     } else {
         attr = [[[NSAttributedString alloc] initWithString:self.text] mutableCopy];
     }
-    
+    // 锁定可以点击文字的范围
     NSRange range = [[attr string] rangeOfString:text];
     
     if (range.location != NSNotFound) {
@@ -103,19 +116,21 @@
     CGPoint point = [touch locationInView:self];
    
     __block BOOL isClickText = NO;
-    
+    // 遍历所有的点击事件
     [self.clickItems enumerateObjectsUsingBlock:^(FMLinkLabelClickItem *obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        [obj.textRects enumerateObjectsUsingBlock:^(NSValue *rectValue, NSUInteger idx, BOOL * _Nonnull stop) {
+        // 遍历点击事件里的点击范围
+        [obj.textRects enumerateObjectsUsingBlock:^(NSValue *rectValue, NSUInteger idx, BOOL * _Nonnull stop1) {
             if (CGRectContainsPoint([rectValue CGRectValue], point)) {
                 if (obj.clickBlock) {
                     obj.clickBlock(obj.transmitBody);
                     isClickText = YES;
                     *stop = YES;
+                    *stop1 = YES;
                 }
             }
         }];
     }];
-    
+    // 遍历点击图片  有待完善
     if (!isClickText) {
         if (self.attributedText) {
             __weak typeof(self)weakSelf = self;
